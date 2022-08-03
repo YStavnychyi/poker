5f12e903eb942106b0477ceb2fb3dc3c675712ca
Yevhenii <zhestav20@gmail.com>
2022-08-03 19:31:35 +0200

    Ability to choose cards

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Card} from "react-bootstrap";
 
 const GameCard = (card) => {
+
+    const [isChoose, setIsChoose] = useState(false)
+
+    const chooseCard = () => {
+        setIsChoose(!isChoose)
+    }
+
     return (
-        <Card className='mx-2' style={card.suit === '♦' || card.suit === '♥' ? {color: "red", border: "1px solid red", width: "45rem"} : {width: "45rem"}}>
-            <Card.Body >
+        <Card onClick={chooseCard} className='mx-2' style={card.suit === '♦' || card.suit === '♥' ? {
+            color: "red",
+            border: "1px solid red",
+            width: "45rem"
+        } : {width: "45rem"}}
+        >
+            <Card.Body style={isChoose ? {border: "1px solid gray", boxShadow: "0px 0px 2px 3px #aaa"} : undefined}>
                 <Card.Title>{card.suit}</Card.Title>
                 <Card.Title>{card.symbol}</Card.Title>
                 <Card.Title>{card.value}</Card.Title>
